0fe8cbf37133a1fa88fd763d49d3703d9c1f19d4
Marcos Diaz <marcos.dure.diaz@gmail.com>
2019-10-21 13:26:17 -0300

    move depois de rot parou de funcionar

--- LAB9/lab9.s
+++ LAB9/lab9.s
@@ -8,18 +8,15 @@ main:
 # inicializa a pilha
 li sp, 40000
 
+li a0, 270
+jal rotate
+
 li a0, 20
 jal move
 
-li s1, 50
-printLoop:
-	jal printCoordenadas
-	addi s1, s1, -1
-	bne zero, s1, printLoop
-
-    li a0, 0 # exit code
-    li a7, 93 # syscall exit
-    ecall
+li a0, 0 # exit code
+li a7, 93 # syscall exit
+ecall
 
 
 # anda a0 cm para frente
@@ -32,13 +29,29 @@ move:
 
 # roda a0 graus no sentido anti-horario
 rotate:
+    # empilha ra
+    addi sp, sp, -4
+	sw ra, 0(sp)
+    # chama a rotacao
     mv a1, a0
     li a7, 2100
     li a0, 2
     ecall
+    # espera a rotacao terminar
+    rotwaitLoop:
+        jal getRot
+        beq a0, s1, rotsaiLoop
+        mv s1, a0 # guarda a ultima rot
+        jal printCoordenadas
+        jal wait1sec
+        j rotwaitLoop
+    rotsaiLoop:
+    # desempilha ra
+    lw ra, 0(sp)
+	addi sp, sp, 4
     ret
 
-getTime:
+getTime_mSec:
   la a0, buffer_timeval
   la a1, buffer_timerzone
   li a7, 169 # chamada de sistema gettimeofday
@@ -52,31 +65,47 @@ getTime:
   add a0, t2, t1
   ret
 
+# retorna o tempo em segundos no a0
+getTime_Sec:
+  la a0, buffer_timeval
+  la a1, buffer_timerzone
+  li a7, 169 # chamada de sistema gettimeofday
+  ecall
+  la a0, buffer_timeval
+  lw t1, 0(a0) # tempo em segundos
+  mv a0, t1
+  ret
+
+wait1sec:
+    addi sp, sp, -4
+	sw ra, 0(sp)
+    addi sp, sp, -4
+	sw s0, 0(sp)
+    jal getTime_Sec
+    mv s0, a0
+    addi s0, s0, 1
+    secloop:
+        jal getTime_Sec
+        mv t0, a0
+        bne t0, s0, secloop
+    lw s0, 0(sp)
+	addi sp, sp, 4
+    lw ra, 0(sp)
+	addi sp, sp, 4
+	ret
+
+
 #imprime o tempo em segundos
 printTime:
 	addi sp, sp, -4
 	sw ra, 0(sp)
-	jal getTime
+	jal getTime_Sec
     mv  a1, a0
 	la  a0, mask
     jal printf
 	lw ra, 0(sp)
 	addi sp, sp, 4
 	ret
-
-printTest:
-	addi sp, sp, -4
-	sw ra, 0(sp)
-    li  a7, 2104
-    li  a0, 0
-    ecall
-    mv  a1, a0
-    la  a0, mask
-    jal printf
-	lw ra, 0(sp)
-	addi sp, sp, 4
-	ret
-
  
 #imprime as cordenadas
 printCoordenadas:
@@ -85,24 +114,68 @@ printCoordenadas:
 	li  a7, 2104
     li  a0, 0
     ecall
-	la t0, rotMask
-	la t1, xMask
-	la t2, zMask
-	and a1, t0, a0
-	srli a1, a1, 20
-	and a2, t1, a0
-	srli a1, a1, 10
-	and a3, t2, a0
+    # recebe valores
+    jal getTime_Sec
+    mv s1, a0
+	jal getX
+    mv s2, a0
+    jal getZ
+    mv s3, a0
+    # carrega parametros
+    mv a1, s1
+    mv a2, s2
+    mv a3, s3
 	la a0, mask2
+    
 	jal printf
 	lw ra, 0(sp)
 	addi sp, sp, 4
 	ret
 
+# retorna a rotacao em graus em a0
+getRot:
+    li  a7, 2104
+    li  a0, 0
+    ecall
+	la t0, rotMask
+    lw t0, 0(t0)
+	and a1, t0, a0
+	srli a1, a1, 20
+    mv a0, a1
+    ret
+
+# retorna a rotacao em graus em a0
+getX:
+    li  a7, 2104
+    li  a0, 0
+    ecall
+	la t0, xMask
+    lw t0, 0(t0)
+	and a1, t0, a0
+	srli a1, a1, 10
+    mv a0, a1
+    ret
+
+# retorna a rotacao em graus em a0
+getZ:
+    li  a7, 2104
+    li  a0, 0
+    ecall
+	la t0, zMask
+    lw t0, 0(t0)
+	and a1, t0, a0
+    mv a0, a1
+    ret
+
+
 mask: .ascii "%x\n"
 space: .skip 50
-mask2: .ascii "%rot: %d x: %d z: %d\n"
-
+# segundos, x, z
+mask2: .ascii "[%d.0] (%d, %d)\n"
+space2: .skip 50
+mask3: .ascii "r: %d\n"
+space3: .skip 50
+mask4: .ascii "t: %d\n"
 
 rotMask: .word 4293918720
 xMask: .word 1047552
